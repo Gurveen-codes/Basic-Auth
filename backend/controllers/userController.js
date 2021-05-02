803ea7c790c660a92b292045e6bb6078ccb4b5a3
Gurveen <gurveens11@gmail.com>
2021-05-02 20:23:35 +0530

    Add user login functionality backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,24 @@
 import asyncHandler from "express-async-handler";
+import User from "../model/userModel.js";
 
-const registerUser = asyncHandler(async (req, res) => {});
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const alreadyExist = await User.find({ email });
+
+	if (alreadyExist) {
+		res.status(400);
+		throw new Error("User already exist");
+	} else {
+		const user = await User.create({ name, email, password });
+		if (!user) {
+			res.status(400);
+			throw new Error("Invalid user details");
+		}
+		res.status(201);
+		res.json(user);
+	}
+});
 
 const loginUser = asyncHandler(async (req, res) => {});
 
